fc8ae49d35a1d0bb1db90fcffa0d1761c329b947
Ben Barber <ben@vivum.ai>
2025-03-03 15:04:14 -0500

    use self type for return, require python 3.11

--- ctrnn.pyi
+++ ctrnn.pyi
@@ -1,3 +1,4 @@
+from typing import Self
 import numpy as np
 from numpy.typing import NDArray
 
@@ -14,39 +15,47 @@ class CTRNN:
     total_size: int
     def __new__(
         cls, input_size: int, hidden_size: int, output_size: int, step_size: float = 0.1
-    ) -> CTRNN: ...
+    ) -> Self: ...
     def euler_step(self, inputs: NDArray[np.float64]) -> NDArray[np.float64]:
         """
         Perform an Euler integration step with external inputs
         """
+
     def output(self) -> NDArray[np.float64]:
         """
         Get CTRNN output
         """
+
     def to_file(self, filename: str) -> None:
         """
         Save parameters to a json file
         """
+
     @classmethod
-    def from_file(cls, filename: str) -> CTRNN:
+    def from_file(cls, filename: str) -> Self:
         """
         Load parameters from a json file
         """
+
     def load_genome_json(self, genome: str) -> None:
         """
         Load a squid genome from a json string
         """
+
     def burn_in(self, steps: int) -> None:
         """
         Apply `steps` number of Euler steps with no external input
         """
+
     def reset(self) -> None:
         """
         Reset CTRNN state
         """
-    def clone(self) -> CTRNN:
+
+    def clone(self) -> Self:
         """
         Clone this instance
         """
+
     def __getstate__(self) -> bytes: ...
     def __setstate__(self, state: bytes) -> None: ...
